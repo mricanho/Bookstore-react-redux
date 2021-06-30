c583ee77aeb6c5fc3e3e7e33f0d4ad96107f26e7
Miguel Ricaño <mricanho@gmail.com>
2021-06-30 14:28:27 -0500

    Add book structure

---
+++ src/components/Book.js
@@ -0,0 +1,21 @@
+import PropTypes from 'prop-types';
+
+const Book = ({ book }) => (
+  <table className="table">
+    <tr>
+      <td>{book.id}</td>
+      <td>{book.title}</td>
+      <td>{book.category}</td>
+    </tr>
+  </table>
+);
+
+Book.propTypes = {
+  book: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired,
+    category: PropTypes.string.isRequired,
+  })
+};
+
+export default Book;
---
+++ src/containers/BookList.js
@@ -0,0 +1,2 @@
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
