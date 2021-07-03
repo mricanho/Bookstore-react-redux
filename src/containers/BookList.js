f77984d82b1c7d5fa9caa4c3d130524b3024f0f4
Miguel Ricaño <mricanho@gmail.com>
2021-07-02 19:29:10 -0500

    Add handleFilterChange and selectedCategory to Booklist

--- src/containers/BookList.js
+++ src/containers/BookList.js
@@ -1,43 +1,80 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { deleteBook } from '../actions';
+import { deleteBook, changeFilter } from '../actions';
 import Book from '../components/Book';
+import CategoryFilter from '../components/CategoryFilter';
 
-const BooksList = ({ books, deleteBook }) => {
+const BooksList = ({
+  books, deleteBook, booksFiltered, changeFilter,
+}) => {
   const handleBookRemove = (book) => {
     deleteBook(book);
   };
+  const handleFilterChange = (category) => {
+    changeFilter(category);
+  };
+  const selectedCategory = () => {
+    if (books.filtered !== 'All') {
+      books.book.map(
+        (book) => book.category.toLowerCase() === booksFiltered.toLowerCase(),
+      );
+      return books.book.filter((book) => book.category === booksFiltered);
+    }
+    return books.book;
+  };
+
   return (
 
-    <table>
+    <>
+      <nav className="navbar container">
+        <div className="navbar-brand">
+          <a className="navbar-item" href="https://bulma.io">
+            BOOK STORE
+          </a>
+        </div>
+        <div className="navbar-menu">
+          <div className="navbar-start">
+            <div className="navbar-item">
+              <a href="#!">Books</a>
+            </div>
+            <div className="navbar-item">
+              <CategoryFilter selection={handleFilterChange} />
+            </div>
+          </div>
+          <div className="navbar-end">
+            l
+          </div>
+        </div>
+      </nav>
 
-      <thead>
-        <tr>
-          <th>Book Id</th>
-          <th>Title</th>
-          <th>Category</th>
-          <th>Action</th>
-        </tr>
-      </thead>
-      <tbody>
-        {books.bookReducer.map((book) => (
+      <div className="container card">
+        {selectedCategory().map((book) => (
 
-          <Book key={book.id} book={book} handleBookRemove={handleBookRemove} />
-
+          <Book
+            key={book.id}
+            book={book}
+            handleBookRemove={handleBookRemove}
+          />
         ))}
-      </tbody>
+      </div>
 
-      <tbody />
-    </table>
+    </>
   );
 };
 
-const mapStateToProps = (state) => ({ books: state });
+const mapStateToProps = (state) => ({
+  books: state,
+  booksFiltered: state.filtered,
+});
 
 BooksList.propTypes = {
-  books: PropTypes.objectOf(PropTypes.array).isRequired,
+  books: PropTypes.objectOf(Object).isRequired,
   deleteBook: PropTypes.func.isRequired,
+  changeFilter: PropTypes.func.isRequired,
+  booksFiltered: PropTypes.string.isRequired,
 };
 
-export default connect(mapStateToProps, { deleteBook })(BooksList);
+export default connect(mapStateToProps, { deleteBook, changeFilter })(
+  BooksList,
+);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,10 @@
 import { combineReducers } from 'redux';
-
 import bookReducer from './book';
+import filterReducer from './filter';
 
 const rootReducer = combineReducers({
-  bookReducer,
+  book: bookReducer,
+  filtered: filterReducer,
 });
 
 export default rootReducer;
